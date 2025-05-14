a11dd01c328c9275a057df830942922a5bb6a54b
Krishal-1920 <krishalmodi171@outlook.com>
2025-05-14 14:19:00 +0530

    Complete

--- src/main/java/com/example/Blogzy/repository/CommentLikeRepository.java
+++ src/main/java/com/example/Blogzy/repository/CommentLikeRepository.java
@@ -3,7 +3,6 @@ package com.example.Blogzy.repository;
 import com.example.Blogzy.entity.CommentLike;
 import com.example.Blogzy.entity.Comments;
 import com.example.Blogzy.entity.Replies;
-import com.example.Blogzy.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, String> {
